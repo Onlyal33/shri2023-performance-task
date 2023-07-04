c006d54f7ba571eb34aea5ffa535699ff279b959
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-04 23:29:32 +0200

    moved to vite

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './App.jsx'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+import './reset.css'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header />
+    <Main />
+    <Footer />
+  </React.StrictMode>,
+)
