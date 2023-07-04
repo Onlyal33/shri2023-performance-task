1c764f980c9da542fef31dc6c0da868f344e1b5d
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-05 01:58:10 +0200

    styles back to html

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Main from './App.jsx'
 import Header from './Header.jsx'
-import './reset.css'
-import './styles.css'
+//import './reset.css'
+//import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
