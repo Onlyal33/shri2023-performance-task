56a7fe8faae4b0b2b7dfbc617df473f9630fa84d
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-05 14:42:11 +0200

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 import Main from './App.jsx'
 import Header from './Header.jsx'
-//import './reset.css'
-//import './styles.css'
 
 createRoot(document.getElementById('app')).render(
   <React.StrictMode>
