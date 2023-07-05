d154df3324ae50cd75020389286e7c821a7d1bb9
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-06 00:51:24 +0200

    Revert "init refactoring"
    This reverts commit 4fa7c81de4fd3e8923ba289ccc9064316e186bcc.

--- src/App.jsx
+++ src/App.jsx
@@ -98,14 +98,21 @@ export default function Main() {
 }
 
 function MainDevices() {
+    const initedRef = useRef(false);
     const sumWidthRef = useRef(0);
-    const [activeTab, setActiveTab] = useState('all');
+    const [activeTab, setActiveTab] = useState('');
+
+    useEffect(() => {
+        if (!activeTab && !initedRef.current) {
+            initedRef.current = true;
+            setActiveTab(new URLSearchParams(location.search).get('tab') || 'all');
+        }
+    }, [activeTab]);
 
     const onSelectInput = event => {
         sumWidthRef.current = 0;
         setActiveTab(event.target.value);
     };
-
     return(
         <section className="section main__devices">
         <div className="section__title">
@@ -156,6 +163,7 @@ function TabPanel({ activeTab, sumWidthRef }) {
     };
 
     useEffect(() => {
+        console.log(sumWidthRef.current)
         const newHasRightScroll = sumWidthRef.current > ref.current.offsetWidth;
             setHasRightScroll(newHasRightScroll);
     }, [activeTab]);
