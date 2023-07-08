75d3be8c4d752144b159a9bcc68d51cf72cbaa00
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-07-08 22:24:02 +0200

    rm brotli

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react(), splitVendorChunkPlugin(), [viteCompression({algorithm: 'brotliCompress'})]],
+    plugins: [react(), splitVendorChunkPlugin()],
     base: '/',
-    build: {
-/*       rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react'],
-            reactDom: ['react-dom'],
-          }
-        }
-      }, */
-    },
   }
 
   if (command !== 'serve') {
